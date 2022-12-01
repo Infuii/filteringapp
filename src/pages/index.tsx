98acbbdb45a02b0cb2f62405cdcba93ef7a11d36
abstdast <ayaanraoaaa@gmail.com>
2022-12-01 13:57:02 -0500

    new mapped over stars

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -9,33 +9,43 @@ const Home: NextPage = () => {
     {
       name: "McDonalds",
       category: "Restaurant",
+      star: "5",
     },
     {
       name: "Wendys",
       category: "Restaurant",
+      star: "3",
     },
     {
       name: "Cold Stone",
       category: "Dessert",
+      star: "3",
     },
     {
       name: "Mikes Pastry",
       category: "Dessert",
+      star: "3",
     },
     {
       name: "McDonalds",
       category: "Restaurant",
+      star: "3",
     },
     {
       name: "Walmart",
       category: "Store",
+      star: "5",
     },
   ]);
   const categories = ["Restaurant", "Dessert", "Store"];
+  const stars = ["1", "2", "3", "4", "5"];
 
   const [filtered, setFiltered] = useState<string[]>([]);
   const filteredItems = items.filter((item) =>
     filtered.some((f) => f == item.category)
+  );
+  const filteredRatings = items.filter((item) =>
+    filtered.some((f) => f == item.star)
   );
 
   return (
@@ -65,6 +75,23 @@ const Home: NextPage = () => {
               {element}
             </button>
           ))}
+          {stars.map((element, index) => (
+            <button
+              className={`my-1 mx-2 flex flex-col justify-center rounded border border-solid border-black px-2 py-1 ${
+                filtered.includes(element) && "bg-black text-white"
+              }`}
+              onClick={() => {
+                if (filtered.some((f) => f == element)) {
+                  setFiltered(filtered.filter((f) => f !== element));
+                } else {
+                  setFiltered([element, ...filtered]);
+                }
+              }}
+              key={`filters-${index}`}
+            >
+              {element}
+            </button>
+          ))}
         </div>
 
         <div className="mt-5 flex flex-wrap items-center justify-center">
@@ -73,6 +100,14 @@ const Home: NextPage = () => {
               <div key={`items-${index}`} className="border">
                 <p className="">{item.category}</p>
                 <p className="m-5 flex justify-between">{item.name}</p>
+                <p>{item.star}</p>
+              </div>
+            )
+          )}
+          {(filteredRatings.length > 0 ? filteredRatings : items).map(
+            (item, index) => (
+              <div key={`items-${index}`} className="border">
+                <p>{item.star}</p>
               </div>
             )
           )}
